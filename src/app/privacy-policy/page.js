82d1d1bd22394d99a5b8e0d0f28161345825cca5
Andre Floquet <andrefloquet@gmail.com>
2025-10-31 13:03:48 +1000

    Privacy Policy

---
+++ src/app/privacy-policy/page.js
@@ -0,0 +1,14 @@
+import { CircleCheck } from "lucide-react";
+
+export default function About() {
+    return (
+        <main>
+            <section className="max-w-9/10 mx-auto my-20">
+                <p>Privacy Policy for Floquet App</p><br />
+                <p>Effective date: 01/11/2025</p><br />
+                <p>Andre Floquet built the "Floquet" mobile app as a free service. This App is intended to display my professional profile, including skills, work experience, certifications and education. The App does **not** collect, store, or share any personal data from users. No analytics, no ads, no external tracking.</p><br />
+                <p>If you have any questions or suggestions about this Privacy Policy, please contact me at andrefloquet@gmail.com.</p><br />
+            </section>
+        </main>
+    )
+}
