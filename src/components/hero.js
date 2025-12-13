bc934d8afd33b4263d2f2c28aaced58a46578ab6
Andre Floquet <andrefloquet@gmail.com>
2025-12-13 14:36:04 +1000

    Make the buttons works for downloading resume and cover letter

--- src/components/hero.js
+++ src/components/hero.js
@@ -11,8 +11,12 @@ export default function Hero() {
                     <h1 className="text-6xl text-sky-900 font-bold mb-10">Andre Floquet, The Software Engineer your Team Needs</h1>
                     <p className="text-lg mb-10">Would you rather have an average Permanent Resident developer or a Senior Engineer who is currently holding a student visa? In over 18 years working as a Software Engineer, there was not anything that I could not do yet.</p>
                     <div className='flex flex-col gap-4 justify-center mx-20 md:flex-row md:justify-start md:mx-0'>
-                        <Button href='#' className='text-white'>Resume  &darr;</Button>
-                        <Button href='#' className='bg-white text-gray-800'>Letter &darr;</Button>
+                        <a className='bg-sky-500 text-lg py-4 px-8 rounded-lg text-white' href="/Andre_Floquet_Resume.pdf" download>
+                            Resume  &darr;
+                        </a>
+                        <a className='bg-white text-lg py-4 px-8 rounded-lg text-gray-800' href="/Andre_Floquet_Letter.pdf" download>
+                            Letter &darr;
+                        </a>
                     </div>
                 </div>
                 <div className="mt-10 mx-4 self-end md:mx-0 md:mt-0">
