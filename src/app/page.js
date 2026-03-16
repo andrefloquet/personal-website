b61f2f67a41361143ff1c222ee3b3518ce409910
Andre Floquet <andrefloquet@gmail.com>
2026-03-16 21:55:16 +1000

    feat: Integrate WorkExperience on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from '@/components/hero';
 import How from '@/components/how';
+import WorkExperience from '@/components/work-experience';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <How />
+      <WorkExperience />
     </>
   );
 }
