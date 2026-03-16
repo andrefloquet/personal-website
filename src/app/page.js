050235b8fd95e897b764443b819c210bde911195
Andre Floquet <andrefloquet@gmail.com>
2026-03-16 22:10:15 +1000

    feat: Integrate Education section into homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Hero from '@/components/hero';
 import How from '@/components/how';
 import WorkExperience from '@/components/work-experience';
+import Education from '@/components/education';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <How />
       <WorkExperience />
+      <Education />
     </>
   );
 }
