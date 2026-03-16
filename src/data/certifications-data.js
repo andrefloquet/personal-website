ecaeb495b54708b6287d2900b31298663c0ce4c7
Andre Floquet <andrefloquet@gmail.com>
2026-03-16 22:39:49 +1000

    feat: Add certifications data

---
+++ src/data/certifications-data.js
@@ -0,0 +1,32 @@
+export const CERTIFICATIONS = [
+  {
+    name: "Linux Essentials 010-060",
+    issuer: "Linux Professional Institute (LPI)",
+    issued: "Feb 2025",
+    credentialId: "LPI000643212",
+  },
+  {
+    name: "HTML Developer - Excellence",
+    issuer: "W3Schools.com",
+    issued: "Feb 2021",
+    credentialId: "Score: 69/70",
+  },
+  {
+    name: "CSS Developer",
+    issuer: "W3Schools.com",
+    issued: "Mar 2021",
+    credentialId: "Score: 66/70",
+  },
+  {
+    name: "PHP Developer",
+    issuer: "W3Schools.com",
+    issued: "Jan 2021",
+    credentialId: "Score: 63/70",
+  },
+  {
+    name: "1Z0-803 Java SE 7 Programmer I",
+    issuer: "Oracle",
+    issued: "Feb 2015",
+    credentialId: "OC1446079",
+  },
+];
