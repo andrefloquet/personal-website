b304d3e140ad2990d3ccf9d3f0f81d7514cb077a
Andre Floquet <andrefloquet@gmail.com>
2026-03-22 13:51:07 +1000

    feat: Add Contact section to the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import How from '@/components/how';
 import WorkExperience from '@/components/work-experience';
 import Education from '@/components/education';
 import Certifications from '@/components/certifications';
+import Contact from '@/components/contact';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <WorkExperience />
       <Education />
       <Certifications />
+      <Contact />
     </>
   );
 }
